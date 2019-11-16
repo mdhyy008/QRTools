ab8533fafe1489b3a89990a23fd1ded7cc4235c0
dabai2017 <1904812793@qq.com>
2019-11-16 16:15:23 +0800

    增加 王者荣耀扫描小提示

--- app/src/main/java/com/dabai/qrtools/activity/QQshare2Activity.java
+++ app/src/main/java/com/dabai/qrtools/activity/QQshare2Activity.java
@@ -3,10 +3,16 @@ package com.dabai.qrtools.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import com.dabai.qrtools.R;
 import com.dabai.qrtools.TextQRActivity;
 import com.dabai.qrtools.utils.Base64;
 
@@ -33,6 +39,30 @@ public class QQshare2Activity extends Activity {
                 String link = base64.decode(base64_link);
                 finish();
                 ToRes(link);
+
+
+               /*
+                Toast toast = Toast.makeText(this, "邀请好友使用QQ扫一扫即可", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.TOP, 0, 0);
+                toast.show();
+                */
+
+
+                Display display = getWindowManager().getDefaultDisplay();
+                // 获取屏幕高度
+                int height = display.getHeight()/2;
+                //加载Toast布局
+                View toastRoot = LayoutInflater.from(getApplicationContext()).inflate(R.layout.toast_wangzhe, null);
+                //初始化布局控件
+                TextView mTextView = toastRoot.findViewById(R.id.message);
+
+
+                Toast toast = new Toast(getApplicationContext());
+                toast.setGravity(Gravity.TOP, 0, 100);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(toastRoot);
+                toast.show();
+
             }
         } catch (Exception e) {
             Toast.makeText(this, "王者荣耀链接转二维码异常:\n"+e.getMessage(), Toast.LENGTH_LONG).show();
